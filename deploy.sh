35c1c98492c44bd55c7f5d91a1f2421041a06bb3
LazicDavid <dlazic@deployinc.com>
2015-09-13 23:44:10 +0200

    Modified deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git remote set-url origin git@github.com:DavidLazic/SeeR.git
 rm -rf dist
 gulp build
 git add dist/ && git commit -m "Production deployment."
@@ -8,4 +9,5 @@ git add -A
 git stash save
 git checkout master
 git branch -D gh-pages
+git remote set-url origin git@bitbucket.org:DavidLazic/seer.git
 
